b08dc3d6402e01446762a625760d6d40cc120b62
Kirill Salfetnik <oriole001@gmail.com>
2022-12-03 13:22:53 +0100

    Continue styling the home page

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -7,8 +7,39 @@ const Projects = () => {
             My projects:
         </div>
         <div className="projects-list">
-            Project One
-            Project Two
+            <div className="projects-list-item">
+              Chat & Socialize
+            </div>
+            <div className="projects-list-item">
+              Diet Control
+            </div>
+            <div className="projects-list-item">
+              Finance Manager
+            </div>
+            <div className="projects-list-item">
+              Rock-Paper-Scissors
+            </div>
+            <div className="projects-list-item">
+              Landing page
+            </div>
+            <div className="projects-list-item">
+              Five
+            </div>
+            <div className="projects-list-item">
+              Six
+            </div>
+            <div className="projects-list-item">
+              Seven
+            </div>
+            <div className="projects-list-item">
+              Eight
+            </div>
+            <div className="projects-list-item">
+              Nine
+            </div>
+            <div className="projects-list-item">
+              Ten
+            </div>
         </div>
     </div>
   )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,10 @@ const Home = () => {
   return (
     <div>
         <div className="parallax-container"></div>
+        <div className="greet">
+          Hello, my name is Kirill Salfetnik. I am a software engineer specializing in creating modern web applications. 
+          I am a bachelor of sociology, now I am studying computer science at the university.
+          </div>
         <Projects />
     </div>
   )
